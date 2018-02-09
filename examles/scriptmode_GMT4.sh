43d0daf41770ecf10d902ec3e8e15d623943e896
Jim T. Tang <jim_tang@htc.com>
2018-02-09 12:01:54 +0800

    change GMT4 example scripts

--- examles/scriptmode_GMT4.sh
+++ examles/scriptmode_GMT4.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
-# reference: http://gmt.soest.hawaii.edu/doc/5.4.3/pdf/GMT_Tutorial.pdf
+# reference: http://gmt.soest.hawaii.edu/gmt4/
 
 echo tutorial 01..
-godr psbasemap -R10/70/-3/8 -JX4i/3i -Ba -B+glightred+t"My first plot" -P > GMT_tut_1.ps
+godr psbasemap -R1/10000/1e20/1e25 -JX9il/6il -B2:\~Wavelength\ \(m\)\~:/a1pf3:~Power\ \(W\)\~:WS > GMT4_tut_01.ps
 echo tutorial 02..
-godr psbasemap -R1/10000/1e20/1e25 -JX9il/6il -Bxa2+l"Wavelength (m)" -Bya1pf3+l"Power (W)" -BWS > GMT_tut_2.ps
+godr pscoast -R-90/-70/0/20 -JM6i -P -B5g5 -Gchocolate > GMT4_tut_02.ps
 echo tutorial 03..
-godr pscoast -R-90/-70/0/20 -JM6i -P -Ba -Gchocolate > GMT_tut_3.ps
+godr blockmedian -R245/255/20/30 -I5m -V ship.xyz > ship_5m.xyz
+godr surface ship_5m.xyz -R245/255/20/30 -I5m -Gship.nc -V
+godr psmask -R245/255/20/30 -I5m ship_5m.xyz -JM6i -B2 -P -K -V > GMT4_tut_03.ps
+godr grdcontour ship.nc -J -O -K -C250 -A1000 >> GMT4_tut_03.ps
+godr psmask -C -O >> GMT4_tut_03.ps
 echo tutorial 04..
-godr pscoast -R-130/-70/24/52 -JB-100/35/33/45/6i -Ba -B+t"Conic Projection" -N1/thickest -N2/thinnest -A500 -Ggray -Wthinnest -P > GMT_tut_4.ps
-echo tutorial 05..
-godr pscoast -Rg -JG280/30/6i -Bag -Dc -A5000 -Gwhite -SDarkTurquoise -P > GMT_tut_5.ps
-echo tutorial 06..
-godr pscoast -Rg -JKs180/9i -Bag -Dc -A5000 -Gchocolate -SDarkTurquoise -Wthinnest > GMT_tut_6.ps
-echo tutorial 07..
-godr psxy @tut_data.txt -R0/6/0/6 -Jx1i -P -Baf -Wthinner > GMT_tut_7.ps
-echo tutorial 08..
-godr psxy @tut_data.txt -R0/6/0/6 -Jx1i -Baf -P -K -Wthinner > GMT_tut_8.ps
-godr psxy tut_data.txt -R -J -O -W -Si0.2i >> GMT_tut_8.ps
-echo tutorial 09..
-godr makecpt -Cred,green,blue -T0,70,300,10000 > quakes.cpt
-godr pscoast -R130/150/35/50 -JM6i -B5 -P -Ggray -K > GMT_tut_9.ps
-godr psxy -R -J -O @tut_quakes.ngdc -Wfaint -i4,3,5,6s0.1 -h3 -Scc -Cquakes.cpt >> GMT_tut_9.ps
-echo tutorial 10..
-godr grdcontour @tut_bathy.nc -JM7i -C250 -A1000 -P -Ba > GMT_tut_10.ps
-echo tutorial 11..
-godr nearneighbor -R245/255/20/30 -I5m -S40k -Gship.nc -V @tut_ship.xyz
-godr grdcontour ship.nc -JM6i -P -Ba -C250 -A1000 > GMT_tut_11.ps
+godr makecpt -Crainbow -T-20/60/10 > disc.cpt
+godr makecpt -Crainbow -T-20/60/10 -Z > cont.cpt
+godr psbasemap -R0/8.5/0/11 -Jx1i -P -B0 -K > GMT4_tut_04.ps  
+godr psscale -D3i/3i/4i/0.5ih -Cdisc.cpt -B:discrete: -O -K >> GMT4_tut_04.ps  
+godr psscale -D3i/5i/4i/0.5ih -Ccont.cpt -B:continuous: -O -K >> GMT4_tut_04.ps  
+godr psscale -D3i/7i/4i/0.5ih -Cdisc.cpt -B:discrete: -I0.5 -O -K >> GMT4_tut_04.ps  
+godr psscale -D3i/9i/4i/0.5ih -Ccont.cpt -B:continuous: -I0.5 -O >> GMT4_tut_04.ps
+
 echo Tutorial tests are done.
